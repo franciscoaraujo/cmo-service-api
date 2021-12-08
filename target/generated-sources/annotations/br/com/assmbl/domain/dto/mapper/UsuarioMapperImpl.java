fbcccda7857526f628941f6788c6ed7caaa1db48
franciscoaraujo <chicoaraujo1063@gmail.com>
2021-12-08 18:56:35 -0300

    adicionando login

---
+++ target/generated-sources/annotations/br/com/assmbl/domain/dto/mapper/UsuarioMapperImpl.java
@@ -0,0 +1,46 @@
+package br.com.assmbl.domain.dto.mapper;
+
+import br.com.assmbl.domain.dto.request.UsuarioDTO;
+import br.com.assmbl.domain.model.Usuario;
+import br.com.assmbl.domain.model.Usuario.UsuarioBuilder;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-12-05T02:40:42-0300",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.4.0.v20210708-0430, environment: Java 17.0.1 (Oracle Corporation)"
+)
+@Component
+public class UsuarioMapperImpl implements UsuarioMapper {
+
+    @Override
+    public Usuario toUsuario(UsuarioDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        UsuarioBuilder usuario = Usuario.builder();
+
+        usuario.id( dto.getId() );
+        usuario.password( dto.getPassword() );
+        usuario.username( dto.getUsername() );
+
+        return usuario.build();
+    }
+
+    @Override
+    public UsuarioDTO toUsuarioDTO(Usuario usuario) {
+        if ( usuario == null ) {
+            return null;
+        }
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+
+        usuarioDTO.setId( usuario.getId() );
+        usuarioDTO.setPassword( usuario.getPassword() );
+        usuarioDTO.setUsername( usuario.getUsername() );
+
+        return usuarioDTO;
+    }
+}
